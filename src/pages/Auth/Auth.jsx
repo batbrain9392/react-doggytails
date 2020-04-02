6b1c1c7f6c0864ae2204338893d212eb76e18d0a
batbrain9392 <batbrain9392@gmail.com>
2020-04-03 00:41:21 +0100

    Auth works with AuthContext

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
+
+import AuthContext from '../../lib/auth-context'
 
 const Auth = () => {
-  return <div>Auth</div>
+  const { isAuthenticated, login } = useContext(AuthContext)
+  const history = useHistory()
+  const location = useLocation()
+  const { from } = location.state || { from: { pathname: '/' } }
+  if (isAuthenticated) {
+    history.replace(from)
+  }
+
+  const handleClick = () => {
+    login()
+    history.replace(from)
+  }
+
+  return <button onClick={handleClick}>login</button>
 }
 
 export default Auth
