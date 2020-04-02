2d388e037292e767c62fe13266f0e4ea8f756e6f
batbrain9392 <batbrain9392@gmail.com>
2020-04-02 22:36:48 +0100

    set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
+
+import Home from './pages/Home/Home'
+import Pets from './pages/Pets/Pets'
+import Adopt from './pages/Adopt/Adopt'
+import Donate from './pages/Donate/Donate'
+import Logout from './pages/Auth/Logout/Logout'
+import Auth from './pages/Auth/Auth'
 
 function App() {
-  return <div>App</div>
+  const isAuthenticated = true
+  const routes = !isAuthenticated ? (
+    <Switch>
+      <Route path='/pets' component={Pets} />
+      <Route path='/auth' component={Auth} />
+      <Route path='/' exact component={Home} />
+      <Redirect to='/' />
+    </Switch>
+  ) : (
+    <Switch>
+      <Route path='/pets' component={Pets} />
+      <Route path='/adopt' component={Adopt} />
+      <Route path='/donate' component={Donate} />
+      <Route path='/logout' component={Logout} />
+      <Route path='/auth' component={Auth} />
+      <Route path='/' exact component={Home} />
+      <Redirect to='/' />
+    </Switch>
+  )
+
+  return routes
 }
 
 export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
+const root = (
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
+
+ReactDOM.render(root, document.getElementById('root'))
