8a2a8c024a02892e5f0a133a1489b873af4aa4cb
batbrain9392 <batbrain9392@gmail.com>
2020-04-05 10:18:24 +0100

    cards in Adopt

--- src/pages/Pets/Pets.jsx
+++ src/pages/Pets/Pets.jsx
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { useRouteMatch, Link } from 'react-router-dom'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroupItem from 'react-bootstrap/ListGroupItem'
 
 import petService from '../../http/pet'
+
+import img from '../../dog.jpeg'
 
 const Pets = () => {
   const [pets, setPets] = useState([])
@@ -24,12 +30,26 @@ const Pets = () => {
       {loading ? (
         'Loading...'
       ) : pets.length ? (
-        pets.map(pet => (
-          <div key={pet.id}>
-            <pre>{JSON.stringify(pet, null, 2)}</pre>
-            <Link to={`${url}/${pet.id}`}>view details</Link>
-          </div>
-        ))
+        <section className='pet-grid'>
+          {pets.map(pet => (
+            <Card key={pet.id}>
+              <Card.Img variant='top' src={img} />
+              <Card.Body>
+                <Card.Title>{pet.name}</Card.Title>
+                <Card.Subtitle className='mb-3 text-muted'>
+                  <small>{pet.breed}</small>
+                </Card.Subtitle>
+                <Card.Text>{pet.description}</Card.Text>
+                <Link to={`${url}/${pet.id}`}>View details</Link>
+              </Card.Body>
+              <Card.Footer>
+                <small className='text-muted'>
+                  <strong>Date available: </strong> {pet.dateAvailable}
+                </small>
+              </Card.Footer>
+            </Card>
+          ))}
+        </section>
       ) : (
         <p>
           There are no pets up for adoption now. <br />
