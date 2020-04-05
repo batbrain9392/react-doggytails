11904b481e77170d90faa1531e3c6b77b881b60c
batbrain9392 <batbrain9392@gmail.com>
2020-04-05 09:21:24 +0100

    header sorted

--- src/components/UI/Header/Header.jsx
+++ src/components/UI/Header/Header.jsx
@@ -1,4 +1,8 @@
 import React, { useContext } from 'react'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
 
 import AuthContext from '../../../lib/auth-context'
 
@@ -12,28 +16,32 @@ const Header = () => {
 
   const protectedNavLinks = isAuthenticated ? (
     <>
-      <CustomNavLink to='/my-profile'>My Profile</CustomNavLink>
-      <button onClick={handleClick}>Logout</button>
+      <CustomNavLink to='/my-profile'>MY PROFILE</CustomNavLink>
+      <Button variant='link' as={Nav.Link} onClick={handleClick}>
+        LOGOUT
+      </Button>
     </>
   ) : (
-    <CustomNavLink to='/auth'>Auth</CustomNavLink>
+    <CustomNavLink to='/auth'>AUTH</CustomNavLink>
   )
 
   return (
-    <header
-      style={{
-        display: 'grid',
-        gridAutoFlow: 'column',
-        justifyContent: 'start',
-        gap: '20px',
-      }}>
-      {/* <CustomNavLink to='/' exact>
-        Home
-      </CustomNavLink> */}
-      <CustomNavLink to='/adopt'>Adopt</CustomNavLink>
-      <CustomNavLink to='/donate'>Donate</CustomNavLink>
-      {protectedNavLinks}
-    </header>
+    <Navbar bg='dark' variant='dark' expand='md'>
+      <Container>
+        <Navbar.Brand href='#home'>DoggyTails</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='mr-auto'>
+            {/* <CustomNavLink to='/' exact>
+         Home
+       </CustomNavLink> */}
+            <CustomNavLink to='/adopt'>ADOPT</CustomNavLink>
+            <CustomNavLink to='/donate'>DONATE</CustomNavLink>
+          </Nav>
+          <Nav>{protectedNavLinks}</Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
