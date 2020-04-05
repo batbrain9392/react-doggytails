41fbd238563144c1c1ae4a1dd6623f085e517849
batbrain9392 <batbrain9392@gmail.com>
2020-04-05 20:38:07 +0100

    added logo

--- src/components/UI/Header/Header.jsx
+++ src/components/UI/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { NavLink } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
@@ -7,6 +8,7 @@ import Button from 'react-bootstrap/Button'
 import AuthContext from '../../../lib/auth-context'
 
 import CustomNavLink from '../CustomNavLink/CustomNavLink'
+import logo from '../../../logo.webp'
 
 const Header = () => {
   const { isAuthenticated, logout } = useContext(AuthContext)
@@ -28,17 +30,20 @@ const Header = () => {
   return (
     <Navbar bg='dark' variant='dark' expand='md'>
       <Container>
-        <Navbar.Brand>DoggyTails</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to='/'>
+          <img alt='logo' src={logo} height='50' className='d-inline-block' />{' '}
+          <span style={{ fontFamily: 'cursive' }}>DoggyTails</span>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto'>
+          <Nav className='ml-auto'>
             {/* <CustomNavLink to='/' exact>
          Home
        </CustomNavLink> */}
             <CustomNavLink to='/adopt'>ADOPT</CustomNavLink>
             <CustomNavLink to='/donate'>DONATE</CustomNavLink>
+            {protectedNavLinks}
           </Nav>
-          <Nav>{protectedNavLinks}</Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
