36bc2edf9420540b39c59d867cc0b668039b7ea7
batbrain9392 <batbrain9392@gmail.com>
2020-04-05 10:29:05 +0100

    extracted PetCard

---
+++ src/components/UI/PetCard/PetCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Card from 'react-bootstrap/Card'
+
+import img from '../../../dog.jpeg'
+
+const PetCard = ({ pet, url }) => {
+  return (
+    <Card>
+      <Card.Img variant='top' src={img} />
+      <Card.Body>
+        <Card.Title>{pet.name}</Card.Title>
+        <Card.Subtitle className='mb-3 text-muted'>
+          <small>{pet.breed}</small>
+        </Card.Subtitle>
+        <Card.Text>{pet.description}</Card.Text>
+        <Link to={`${url}/${pet.id}`}>View details</Link>
+      </Card.Body>
+      <Card.Footer>
+        <small className='text-muted'>
+          <strong>Date available: </strong> {pet.dateAvailable}
+        </small>
+      </Card.Footer>
+    </Card>
+  )
+}
+
+export default PetCard
