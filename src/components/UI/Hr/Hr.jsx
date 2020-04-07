823a4df7f432b31566712e252dd5a3a0af81c1e0
batbrain9392 <batbrain9392@gmail.com>
2020-04-07 16:42:01 +0100

    bugfix

--- src/components/UI/Hr/Hr.jsx
+++ src/components/UI/Hr/Hr.jsx
@@ -6,11 +6,7 @@ import { ReactComponent as Star } from '../../../assets/icons/star.svg'
 const Hr = ({ children, width, loading }) => {
   return (
     <div className='Hr mb-5'>
-      {children ? (
-        children
-      ) : (
-        <hr className='m-0' style={{ width: `${width}vh` }} />
-      )}
+      {children ? children : <hr className='m-0' style={{ width }} />}
       <div className='Svg'>
         {loading ? (
           <Spinner animation='grow' variant='secondary' className='m-auto' />
