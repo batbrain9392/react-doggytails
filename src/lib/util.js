3218316e8bbb0acd93152575a0b4adcb6027f026
batbrain9392 <batbrain9392@gmail.com>
2020-04-08 18:09:57 +0100

    added toDateInput

--- src/lib/util.js
+++ src/lib/util.js
@@ -11,3 +11,13 @@ export const toArray = (object) => {
 export const toDateLocale = (datetimeInMilliseconds) => {
   return new Date(datetimeInMilliseconds).toLocaleDateString()
 }
+
+export const toDateInput = (datetimeInMilliseconds) => {
+  const date = new Date(datetimeInMilliseconds)
+  const yyyy = date.getFullYear()
+  let mm = date.getMonth() + 1
+  let dd = date.getDate()
+  mm = mm < 10 ? `0${mm}` : mm
+  dd = dd < 10 ? `0${dd}` : dd
+  return `${yyyy}-${mm}-${dd}`
+}
