63e1ddc068b5d64b761b5f108ac6e327c1978c9d
batbrain9392 <batbrain9392@gmail.com>
2020-04-11 10:05:00 +0100

    css modules added

--- src/components/TopPets/TopPets.jsx
+++ src/components/TopPets/TopPets.jsx
@@ -1,5 +1,7 @@
 import React, { memo } from 'react'
 
+import classes from './TopPets.module.scss'
+
 const TopPets = () => {
   console.log('TopPets')
   return <div></div>
--- src/components/UI/Events/Events.jsx
+++ src/components/UI/Events/Events.jsx
@@ -1,5 +1,7 @@
 import React, { memo } from 'react'
 
+import classes from './Events.module.scss'
+
 const Events = () => {
   return <div></div>
 }
