599487fa5818c4a8476e5c80c76c4e251078a652
batbrain9392 <batbrain9392@gmail.com>
2020-04-15 14:44:59 +0100

    generalisation

---
+++ src/components/UI/AdminDataPanel/AdminDataPanel.jsx
@@ -0,0 +1,37 @@
+import React, { memo } from 'react'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
+import Badge from 'react-bootstrap/Badge'
+
+const AdminDataPanel = ({ data }) => {
+  return (
+    <div className='mt-4'>
+      {!data.length ? (
+        'No data present'
+      ) : (
+        <>
+          <h5 className='mb-4'>
+            Total items in list:{' '}
+            <Badge variant='secondary'>{data.length}</Badge>
+          </h5>
+          <Accordion defaultActiveKey={data[0].id}>
+            {data.map((item) => (
+              <Card key={item.id}>
+                <Accordion.Toggle as={Card.Header} eventKey={item.id}>
+                  {item.name}
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey={item.id}>
+                  <Card.Body>
+                    <pre>{JSON.stringify(item, null, 2)}</pre>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            ))}
+          </Accordion>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default memo(AdminDataPanel)
--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,14 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useCallback, useEffect } from 'react'
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
 
+import AuthContext from '../../lib/auth-context'
+import petService from '../../http/pet'
+import userService from '../../http/user'
+
 import Heading from '../../components/UI/Heading/Heading'
-import AdminDataPets from '../../components/AdminDataPets/AdminDataPets'
-import AdminDataUsers from '../../components/AdminDataUsers/AdminDataUsers'
+import AdminDataPanel from '../../components/UI/AdminDataPanel/AdminDataPanel'
 
 const Admin = () => {
+  const [pets, setPets] = useState([])
   const [loadingPets, setLoadingPets] = useState(true)
+  const [users, setUsers] = useState([])
   const [loadingUsers, setLoadingUsers] = useState(true)
+  const { token } = useContext(AuthContext)
+
+  const fetchAllPets = useCallback(async () => {
+    try {
+      const data = await petService.fetchAll()
+      setPets(data)
+      setLoadingPets(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+
+  const fetchAllUsers = useCallback(async () => {
+    try {
+      const data = await userService.fetchAll(token)
+      setUsers(data)
+      setLoadingUsers(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }, [token])
+
+  useEffect(() => {
+    fetchAllPets()
+    fetchAllUsers()
+  }, [fetchAllPets, fetchAllUsers])
+
   const type = {
     pets: 'Pets',
     users: 'Users',
@@ -19,10 +51,10 @@ const Admin = () => {
       <Heading loading={loadingPets || loadingUsers}>Admin</Heading>
       <Tabs defaultActiveKey={type.pets} id='admin-data'>
         <Tab eventKey={type.pets} title={type.pets}>
-          <AdminDataPets loading={loadingPets} setLoading={setLoadingPets} />
+          {!loadingPets && <AdminDataPanel data={pets} />}
         </Tab>
         <Tab eventKey={type.users} title={type.users}>
-          <AdminDataUsers loading={loadingUsers} setLoading={setLoadingUsers} />
+          {!loadingUsers && <AdminDataPanel data={users} />}
         </Tab>
       </Tabs>
     </>
