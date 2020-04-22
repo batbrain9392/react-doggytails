43df7194cb8768e56471a66c17cf6e11b14a2b32
batbrain9392 <batbrain9392@gmail.com>
2020-04-22 22:36:19 +0100

    fetching messages for admin

--- src/http/message.js
+++ src/http/message.js
@@ -1,6 +1,14 @@
 import db from './db'
+import { toArray } from '../lib/util'
 
 const url = 'messages'
+const addToken = (token) => `auth=${token}`
+
+const fetchAll = async (token) => {
+  const queryParams = addToken(token)
+  const { data } = await db.get(`/${url}.json?${queryParams}`)
+  return toArray(data)
+}
 
 const send = async (message) => {
   const { data } = await db.post(`/${url}.json`, message)
@@ -8,5 +16,6 @@ const send = async (message) => {
 }
 
 export default {
+  fetchAll,
   send,
 }
